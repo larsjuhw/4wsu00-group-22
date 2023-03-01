d3f54e7d70ee996a57847a415e9a392fa6a6ce7f
Lars Wolter <larswolter@me.com>
2023-03-01 20:31:44 +0100

    First code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <FlowMeter.h>
 
-void setup() {
-  // put your setup code here, to run once:
+FlowMeter *Meter;
+const byte sensorPin = 2;
+const unsigned long delayPeriod = 1000;
+
+void SensorISR() { Meter->count(); }
+// attachInterrupt
+void setup()
+{
+    Serial.begin(9600);
+    Meter =
+        new FlowMeter(digitalPinToInterrupt(sensorPin), UncalibratedSensor, SensorISR, RISING);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    delay(delayPeriod);
+
+    Meter->tick(delayPeriod);
+    Serial.println("Flow rate: " + String(Meter->getCurrentFlowrate()) + " l/min");
+    Serial.println("Total flow: " + String(Meter->getTotalVolume()) + " l");
 }
